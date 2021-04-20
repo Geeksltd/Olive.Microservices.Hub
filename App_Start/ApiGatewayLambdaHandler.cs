1051c3f23116141c5a171f2c05f969b5eb6ef187
PaymonK <35069374+PaymonK@users.noreply.github.com>
2021-04-20 20:05:13 +0100

    Delete ApiGatewayLambdaHandler.cs

--- App_Start/ApiGatewayLambdaHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Olive.Microservices.Hub
-{
-    public class ApiGatewayLambdaHandler<TTaskManager> : FS.Shared.Website.ApiGatewayLambdaHandler<HubStartup<TTaskManager>> where TTaskManager : BackgroundJobsPlan, new()
-    {
-    }
-}
