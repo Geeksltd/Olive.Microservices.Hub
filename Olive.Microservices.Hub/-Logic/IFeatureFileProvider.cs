c33b5213d46a715dcf632573e28d5ba3989a0ac4
Matt <matt@geeks.ltd.uk>
2021-05-24 14:50:37 +0100

    no message

---
+++ Olive.Microservices.Hub/-Logic/IFeatureFileProvider.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Olive.Microservices.Hub;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olive.Microservices.Hub
+{
+    public interface IFeatureFileProvider
+    {
+        Task Save(Service service, string xml);
+        Task<string> Load(Service service);
+    }
+}
---
+++ Olive.Microservices.Hub/Controllers/FeaturesMenuController.cs
@@ -0,0 +1,47 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Olive.Microservices.Hub;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    [Route("features-menu")]
+    public class FeaturesMenuController : Controller
+    {
+        IFeatureFileProvider FeatureFileProvider;
+
+        public FeaturesMenuController(IFeatureFileProvider featureFileProvider)
+        {
+            FeatureFileProvider = featureFileProvider;
+        }
+        public class FeaturesUploadArgs
+        {
+            public string ServiceName, Xml;
+        }
+        [Route("upload")]
+        [HttpPost]
+        public async Task<IActionResult> UploadMenu([FromBody] FeaturesUploadArgs args)
+        {
+            try
+            {
+
+                var service = Service.FindByName(args.ServiceName) ?? throw new Exception($"Could not find a service with {args.ServiceName} name.");
+
+                await FeatureFileProvider.Save(service, args.Xml);
+
+                StructureDeserializer.Load();
+
+                //await Database.Update(Domain.Settings.Current, s => s.FeaturesMenuUpdated = Olive.LocalTime.Now);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
