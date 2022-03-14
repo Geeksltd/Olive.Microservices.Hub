e2f4d30677fb799c914929113309a9eab6ea61c0
PaymonK <35069374+PaymonK@users.noreply.github.com>
2022-03-14 08:01:41 +0000

    Update FeatureDefinition.cs

--- Domain/Logic/Deserialize/FeatureDefinition.cs
+++ Domain/Logic/Deserialize/FeatureDefinition.cs
@@ -20,6 +20,7 @@ namespace Olive.Microservices.Hub
                 Ref = Refrance.OrEmpty(),
                 Title = FullPath.Split("/").LastOrDefault(),
                 Description = Description.OrEmpty(),
+                UseIframe = UseIframe,
                 ImplementationUrl = RelativeUrl.OrEmpty(),
                 BadgeUrl = BadgeUrl,
                 Icon = Icon.OrEmpty(),
