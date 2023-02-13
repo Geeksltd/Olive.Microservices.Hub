f48f7e8bddd22f32d3687784332a5610ffaafe7f
m-hoseinzadeh <mohammad.hoseinzadehashraf@geeks.ltd.uk>
2023-02-13 11:14:27 +0000

    add theme provider

---
+++ Domain/Utilities/ThemeProvider.cs
@@ -0,0 +1,21 @@
+﻿namespace Olive.Microservices.Hub.Domain.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class ThemeProvider
+    {
+        public static string GetRootPath(bool withCurrentTheme)
+        {
+            var root = Microservice.Me.Url().TrimEnd("/");
+            if (root.Contains("hub.")) root = root.Remove("hub.") + "/hub";
+
+            return withCurrentTheme ? $"{root}/themes/{Config.Get("Theme:Name", "default")}" : root;
+        }
+        public static string GetPrimaryColor()
+        {
+            return Config.Get("Theme:PrimaryColor", "#42AAA9");
+        }
+    }
+}
