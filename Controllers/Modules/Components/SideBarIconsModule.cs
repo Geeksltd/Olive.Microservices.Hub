e7c6a9cbd91695c42d6fb8339990e99f0af09b0b
mansour_nazifiasl_geeksltd <mansour.nazifiasl@geeks.ltd.uk>
2023-10-19 07:31:41 +0100

    Add SideBarIconsModule

---
+++ Controllers/Modules/Components/SideBarIconsModule.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Olive.Mvc;
+using vm = ViewModel;
+
+namespace ViewComponents
+{
+#pragma warning disable
+    public partial class SideBarIconsModule : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(vm.SideBarIconsModule info)
+        {
+            return View(await Bind<vm.SideBarIconsModule>(info));
+        }
+    }
+}
+
+namespace Controllers
+{
+#pragma warning disable
+    public partial class SideBarIconsModuleController : BaseController
+    {
+    }
+}
+
+namespace ViewModel
+{
+#pragma warning disable
+    [BindingController(typeof(Controllers.SideBarIconsModuleController))]
+    public partial class SideBarIconsModule : IViewModel
+    {
+    }
+}
