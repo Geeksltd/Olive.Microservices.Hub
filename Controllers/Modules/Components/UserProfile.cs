259792d1f00e0db510bf157c83d10c10a8492847
mohammad_hoseinzadehashraf_geeksltd <mohammad.hoseinzadehashraf@geeks.ltd.uk>
2023-11-17 18:26:19 +0000

    new user-profile component

---
+++ Controllers/Modules/Components/UserProfile.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Olive.Microservices.Hub.Domain.Theme.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Olive;
+using Olive.Mvc;
+using vm = ViewModel;
+
+namespace ViewComponents
+{
+#pragma warning disable
+	public partial class UserProfile : ViewComponent
+	{
+        private readonly IThemeProvider _themeProvider;
+
+        public UserProfile(IThemeProvider themeProvider)
+        {
+            _themeProvider = themeProvider;
+        }
+
+		public async Task<IViewComponentResult> InvokeAsync(vm.UserProfile info)
+		{
+			var email = Context.Current.User().GetEmail();
+
+			var user = await Context.Current.Database().FirstOrDefault<PeopleService.UserInfo>(x => x.Email == email);
+			var userRoles = user.Roles.Split(',');
+
+			var sidebarProfileUrl = await _themeProvider.GetSidebarProfileUrl(userRoles, new Dictionary<string, string>
+			{
+				{ "ID", user?.ID.ToString().OrEmpty() },
+				{ "EMAIL", email },
+			});
+
+			info = new vm.UserProfile()
+			{
+				Email = email,
+				UserImage = user?.ImageUrl,
+				PrimaryDISCColour = user?.PrimaryDISCColour.Or("transparent"),
+				SecondaryDISCColour = user?.SecondaryDISCColour.Or("transparent"),
+				ProfileUrl = sidebarProfileUrl,
+			};
+
+			return View(info);
+		}
+	}
+}
+
+namespace Controllers
+{
+#pragma warning disable
+	public partial class UserProfileController : BaseController
+	{
+	}
+}
+
+namespace ViewModel
+{
+#pragma warning disable
+	[BindingController(typeof(Controllers.UserProfileController))]
+	public partial class UserProfile : IViewModel
+	{
+		public string Email { get; set; }
+		public string ProfileUrl { get; set; }
+		public string UserImage { get; set; }
+		public string PrimaryDISCColour { get; set; }
+		public string SecondaryDISCColour { get; set; }
+	}
+}
