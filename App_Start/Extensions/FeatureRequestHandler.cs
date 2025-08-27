c6f900a1825cf556e75bd5eb3137896c7989cf9c
Mohammad Hoseinzadeh Ashraf <mohammad.hoseinzadehashraf@geeks.ltd.uk>
2025-08-27 14:43:12 +0100

    update nuget packages

--- App_Start/Extensions/FeatureRequestHandler.cs
+++ App_Start/Extensions/FeatureRequestHandler.cs
@@ -1,134 +1,133 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using Olive.Microservices.Hub.Domain.Theme.Contracts;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
 using Olive;
 using Olive.Microservices.Hub;
+using Olive.Microservices.Hub.Domain.Theme.Contracts;
 using Olive.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ViewModel
 {
-	partial class FeatureView
-	{
-		public string RequestPath, Path, HostAndPath, ExecutionUrl, NewQueryString;
+    partial class FeatureView
+    {
+        public string RequestPath, Path, HostAndPath, ExecutionUrl, NewQueryString;
 
-		internal async Task ReadPath(HttpRequest request)
-		{
-			RequestPath = request.ToPathAndQuery();
-			Path = RequestPath.TrimStart("/");
+        internal async Task ReadPath(HttpRequest request)
+        {
+            RequestPath = request.ToPathAndQuery();
+            Path = RequestPath.TrimStart("/");
 
-			var open = "%5B";
-			var close = "%5D";
+            var open = "%5B";
+            var close = "%5D";
 
-			if (Path.StartsWith(open) && Path.Contains(close))
-			{
-				Path = Path.Substring(3);
-				Path = Path.Remove(Path.IndexOf(close), 3);
-			}
+            if (Path.StartsWith(open) && Path.Contains(close))
+            {
+                Path = Path.Substring(3);
+                Path = Path.Remove(Path.IndexOf(close), 3);
+            }
 
-			HostAndPath = request.RootUrl() + request.Path.ToString().TrimStart("/");
+            HostAndPath = request.RootUrl() + request.Path.ToString().TrimStart("/");
 
-			if (Path.IsEmpty())
-			{
-				var themeProvider = Context.Current.ServiceProvider.GetService<IThemeProvider>();
-				RequestPath = Path = (themeProvider == null ? "dashboard/home.aspx" : await themeProvider.GetHomePageUrl());
-				HostAndPath = request.RootUrl() + Path.TrimStart("/");
-			}
-		}
+            if (Path.IsEmpty())
+            {
+                var themeProvider = Context.Current.GetOptionalService<IThemeProvider>();
+                RequestPath = Path = (themeProvider == null ? "dashboard/home.aspx" : await themeProvider.GetHomePageUrl());
+                HostAndPath = request.RootUrl() + Path.TrimStart("/");
+            }
+        }
 
-		internal Feature FindFeature()
-		{
-			if (Feature.All == null)
-				throw new Exception("Feature.All has not been initialised.");
+        internal Feature FindFeature()
+        {
+            if (Feature.All == null)
+                throw new Exception("Feature.All has not been initialised.");
 
-			return
-				Feature.All.FirstOrDefault(x => x.LoadUrl == Path.ToLower().EnsureStartsWith("/"))
-				?? Feature.FindByHubUrl(Path) ?? Feature.FindByAbsoluteImplementationUrl(HostAndPath)
-				?? Feature.FindBySubFeaturePath(Path);
-		}
-	}
+            return
+                Feature.All.FirstOrDefault(x => x.LoadUrl == Path.ToLower().EnsureStartsWith("/"))
+                ?? Feature.FindByHubUrl(Path) ?? Feature.FindByAbsoluteImplementationUrl(HostAndPath)
+                ?? Feature.FindBySubFeaturePath(Path);
+        }
+    }
 }
 
 namespace Controllers
 {
-	partial class FeatureController
-	{
-		[NonAction, OnBound]
-		public async Task OnBound(ViewModel.FeatureView info)
-		{
-			await info.ReadPath(Request);
-			FeatureContext.ViewingFeature = info.Item = info.FindFeature();
-		}
+    partial class FeatureController
+    {
+        [NonAction, OnBound]
+        public async Task OnBound(ViewModel.FeatureView info)
+        {
+            await info.ReadPath(Request);
+            FeatureContext.ViewingFeature = info.Item = info.FindFeature();
+        }
 
-		async Task<ActionResult> Execute(ViewModel.FeatureView info)
-		{
-			// if (info.RequestPath.StartsWith("/Hub/", caseSensitive: false))
-			//    return Redirect(info.RequestPath.Substring(4));
-			ViewData["NoNav"] = info.NoNav;
-			ViewData["Title"] = info.Item?.GetFullPath();
-			// Log.Error(info.RequestPath + " | " + Request.ToPathAndQuery() + " | " + Request.ToRawUrl());
+        async Task<ActionResult> Execute(ViewModel.FeatureView info)
+        {
+            // if (info.RequestPath.StartsWith("/Hub/", caseSensitive: false))
+            //    return Redirect(info.RequestPath.Substring(4));
+            ViewData["NoNav"] = info.NoNav;
+            ViewData["Title"] = info.Item?.GetFullPath();
+            // Log.Error(info.RequestPath + " | " + Request.ToPathAndQuery() + " | " + Request.ToRawUrl());
 
-			if (info.Item == null)
-			{
-				var path = info.RequestPath.TrimStart("/");
+            if (info.Item == null)
+            {
+                var path = info.RequestPath.TrimStart("/");
 
-				var service = Service.All.FirstOrDefault(s => path.StartsWith(s.Name, caseSensitive: false));
-				if (service is null) return new NotFoundResult();
+                var service = Service.All.FirstOrDefault(s => path.StartsWith(s.Name, caseSensitive: false));
+                if (service is null) return new NotFoundResult();
 
-				var actualRelativeUrl = path.Substring(service.Name.Length).TrimStart("/");
+                var actualRelativeUrl = path.Substring(service.Name.Length).TrimStart("/");
 
-				if (actualRelativeUrl.IsEmpty())
-					return Redirect("/under/" + service.Name);
-				else
-					JavaScript(new JavascriptService("hub", "go", service.GetHubImplementationUrl(actualRelativeUrl), service.UseIframe));
-			}
-			else if (!User.CanSee(info.Item))
-			{
-				return Redirect("/Unauthorized/" + info.Item.ID);
-			}
-			else
-			{
-				if (info.RequestPath.Contains("/%5B"))
-					return Redirect($"/{info.RequestPath.Remove("/%5B").Remove("%5D")}");
+                if (actualRelativeUrl.IsEmpty())
+                    return Redirect("/under/" + service.Name);
+                else
+                    JavaScript(new JavascriptService("hub", "go", service.GetHubImplementationUrl(actualRelativeUrl), service.UseIframe));
+            }
+            else if (!User.CanSee(info.Item))
+            {
+                return Redirect("/Unauthorized/" + info.Item.ID);
+            }
+            else
+            {
+                if (info.RequestPath.Contains("/%5B"))
+                    return Redirect($"/{info.RequestPath.Remove("/%5B").Remove("%5D")}");
 
-				var url = info.RequestPath.Split('/')
-				.Select((v, i) => i == 1 ? v.EnsureStartsWith("[").EnsureEndsWith("]") : v)
-				.ToString("/");
-				// var url = "/[" + info.Item.Service.Name people"]" + info.RequestPath
+                var url = info.RequestPath.Split('/')
+                .Select((v, i) => i == 1 ? v.EnsureStartsWith("[").EnsureEndsWith("]") : v)
+                .ToString("/");
+                // var url = "/[" + info.Item.Service.Name people"]" + info.RequestPath
 
-				JavaScript(new JavascriptService("hub", "go", url, info.Item.UseIframe));
+                JavaScript(new JavascriptService("hub", "go", url, info.Item.UseIframe));
 
-				if (info.Item.ImplementationUrl.HasValue() && info.Item.UseIframe)
-				{
-					JavaScript(new JavascriptService("featuresMenu", "show", info.Item.ID));
-				}
-			}
+                if (info.Item.ImplementationUrl.HasValue() && info.Item.UseIframe)
+                {
+                    JavaScript(new JavascriptService("featuresMenu", "show", info.Item.ID));
+                }
+            }
 
-			ViewBag.PageSource = "";
+            ViewBag.PageSource = "";
 
-			if (info.Path.Contains("/serverside"))
-			{
-				var baseUrl = "";
+            if (info.Path.Contains("/serverside"))
+            {
+                var baseUrl = "";
 
-				if (info.Item is null)
-					baseUrl = Service.All.FirstOrDefault(s => info.Path.StartsWith(s.Name, caseSensitive: false))?.BaseUrl;
-				else
-					baseUrl = info.Item.Service.BaseUrl;
+                if (info.Item is null)
+                    baseUrl = Service.All.FirstOrDefault(s => info.Path.StartsWith(s.Name, caseSensitive: false))?.BaseUrl;
+                else
+                    baseUrl = info.Item.Service.BaseUrl;
 
-				var body = await (baseUrl + info.Path.RemoveBefore("/")).AsUri().Download();
-				ViewBag.PageSource = body;
+                var body = await (baseUrl + info.Path.RemoveBefore("/")).AsUri().Download();
+                ViewBag.PageSource = body;
 
-				var title = body.Between("<!--title", "title-->");
-				if (title.HasValue()) ViewData["Title"] = title;
-				var metaTag = body.Between("<!--metaTag", "metaTag-->");
-				ViewData["metaTag"] = metaTag;
-			}
+                var title = body.Between("<!--title", "title-->");
+                if (title.HasValue()) ViewData["Title"] = title;
+                var metaTag = body.Between("<!--metaTag", "metaTag-->");
+                ViewData["metaTag"] = metaTag;
+            }
 
-			ViewData["LeftMenu"] = "FeaturesSideMenu";
-			return View(info);
-		}
-	}
+            ViewData["LeftMenu"] = "FeaturesSideMenu";
+            return View(info);
+        }
+    }
 }
--- App_Start/HubStartup.cs
+++ App_Start/HubStartup.cs
@@ -2,131 +2,130 @@
 
 namespace Olive.Microservices.Hub
 {
-	using System;
-	using System.Linq;
-	using System.Threading.Tasks;
-	using Microsoft.AspNetCore.Authentication.Google;
-	using Microsoft.AspNetCore.Builder;
-	using Microsoft.AspNetCore.Hosting;
-	using Microsoft.AspNetCore.Mvc.Razor;
-	using Microsoft.Extensions.Configuration;
-	using Microsoft.Extensions.DependencyInjection;
-	using Microsoft.Extensions.Logging;
-	using Olive;
-	using Olive.Entities.Data;
-	using Olive.Microservices.Hub.Domain.Utilities.JsVariable;
+    using Microsoft.AspNetCore.Authentication.Google;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc.Razor;
+    using Microsoft.Extensions.DependencyInjection;
+    using Olive;
+    using Olive.Entities.Data;
+    using Olive.Microservices.Hub.Domain.Utilities.JsVariable;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
 
-	public abstract class HubStartup<TTaskManager> : FS.Shared.Website.Startup<ReferenceData, BackgroundTask, TTaskManager> where TTaskManager : BackgroundJobsPlan, new()
-	{
-		protected HubStartup(IWebHostEnvironment env, IConfiguration config) : base(env, config)
-		{
-			Subdomains = config["HubSubdomain"]?.Split(",") ?? new string[0];
+    public abstract class HubStartup<TTaskManager> : FS.Shared.Website.Startup<ReferenceData, BackgroundTask, TTaskManager> where TTaskManager : BackgroundJobsPlan, new()
+    {
+        protected HubStartup(IWebHostEnvironment env) : base(env)
+        {
+            var config = Config.Build();
+            Subdomains = config["HubSubdomain"]?.Split(",") ?? new string[0];
 
-			if (env.EnvironmentName != "Development") Features.SetRepository(new S3FeatureRepository());
-			else Features.SetRepository(new IOFeatureRepository());
+            if (env.EnvironmentName != "Development") Features.SetRepository(new S3FeatureRepository());
+            else Features.SetRepository(new IOFeatureRepository());
 
-		}
+        }
 
-		protected virtual bool IsProduction() => false;
+        protected virtual bool IsProduction() => false;
 
-		string[] Subdomains;
+        string[] Subdomains;
 
-		public override void ConfigureServices(IServiceCollection services)
-		{
-			services.AddThemes();
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddThemes();
 
-			services.AddResponseCompression();
+            services.AddResponseCompression();
 
-			if (Subdomains.Any())
-				services.AddCors(c => c.AddPolicy("AllowSubdomains", builder =>
-				{
-					var domainProtocol = $"http{"s".OnlyWhen(IsProduction())}://*.";
+            if (Subdomains.Any())
+                services.AddCors(c => c.AddPolicy("AllowSubdomains", builder =>
+                {
+                    var domainProtocol = $"http{"s".OnlyWhen(IsProduction())}://*.";
 
-					var domains = from d in Subdomains
-								  let trimmed = d.TrimStart("*").TrimStart(".")
-								  select domainProtocol.WithSuffix(d);
+                    var domains = from d in Subdomains
+                                  let trimmed = d.TrimStart("*").TrimStart(".")
+                                  select domainProtocol.WithSuffix(d);
 
-					builder.WithOrigins(domains.ToArray())
-				.AllowAnyHeader()
-				.AllowAnyMethod()
-				.AllowCredentials()
-				.SetIsOriginAllowedToAllowWildcardSubdomains();
-				}));
+                    builder.WithOrigins(domains.ToArray())
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .SetIsOriginAllowedToAllowWildcardSubdomains();
+                }));
 
-			base.ConfigureServices(services);
+            base.ConfigureServices(services);
 
-			services.AddScoped<IJsVariableProvider, JsVariableProvider>();
+            services.AddScoped<IJsVariableProvider, JsVariableProvider>();
 
-			services.Configure<RazorViewEngineOptions>(options =>
-			{
-				options.ViewLocationExpanders.Clear();
-				options.ViewLocationExpanders.Add(new ThemeViewLocationExpander());
-			});
-		}
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Clear();
+                options.ViewLocationExpanders.Add(new ThemeViewLocationExpander());
+            });
+        }
 
-		public override void Configure(IApplicationBuilder app)
-		{
-			app.UseResponseCompression();
+        public override void Configure(IApplicationBuilder app)
+        {
+            app.UseResponseCompression();
 
-			if (Subdomains.Any())
-				app.UseCors("AllowSubdomains");
+            if (Subdomains.Any())
+                app.UseCors("AllowSubdomains");
 
-			base.Configure(app);
+            base.Configure(app);
 
-			Console.Title = Microservice.Me.Name;
+            Console.Title = Microservice.Me.Name;
 
-			if (Context.Current.Environment().EnvironmentName != "Development")
-			{
-				app.Use(StructureDeserializer.ReloadFeatures);
-				app.Use(StructureDeserializer.ReloadSources);
-				Task.Factory.RunSync(Features.Load);
-				Task.Factory.RunSync(ViewModel.BoardComponents.SetBoardSources);
-				Task.Factory.RunSync(ViewModel.GlobalSearch.SetSearchSources);
-			}
+            if (Context.Current.Environment().EnvironmentName != "Development")
+            {
+                app.Use(StructureDeserializer.ReloadFeatures);
+                app.Use(StructureDeserializer.ReloadSources);
+                Task.Factory.RunSync(Features.Load);
+                Task.Factory.RunSync(ViewModel.BoardComponents.SetBoardSources);
+                Task.Factory.RunSync(ViewModel.GlobalSearch.SetSearchSources);
+            }
 
-			Feature.DataProvider.Register();
-			Service.DataProvider.Register();
+            Feature.DataProvider.Register();
+            Service.DataProvider.Register();
 
-		}
+        }
 
-		protected override void ConfigureMiddlewares(IApplicationBuilder app)
-		{
-			app.UseGlobalSearch<GlobalSearchSource>();
-			// app.Use(RedirectSmartPhone);
-			base.ConfigureMiddlewares(app);
-		}
+        protected override void ConfigureMiddlewares(IApplicationBuilder app)
+        {
+            app.UseGlobalSearch<GlobalSearchSource>();
+            // app.Use(RedirectSmartPhone);
+            base.ConfigureMiddlewares(app);
+        }
 
-		protected override void ConfigureRequestHandlers(IApplicationBuilder app)
-		{
-			StructureDeserializer.Load();
-			base.ConfigureRequestHandlers(app);
-		}
+        protected override void ConfigureRequestHandlers(IApplicationBuilder app)
+        {
+            StructureDeserializer.Load();
+            base.ConfigureRequestHandlers(app);
+        }
 
-		protected abstract void ConfigureDataProtectionProvider(GoogleOptions config);
+        protected abstract void ConfigureDataProtectionProvider(GoogleOptions config);
 
-		static async Task RedirectSmartPhone(Microsoft.AspNetCore.Http.HttpContext context, Func<Task> next)
-		{
-			if (context.Request.Path.Value == "/" && context.Request.IsSmartPhone())
-				context.Response.Redirect("/root");
-			else await next();
-		}
-	}
+        static async Task RedirectSmartPhone(Microsoft.AspNetCore.Http.HttpContext context, Func<Task> next)
+        {
+            if (context.Request.Path.Value == "/" && context.Request.IsSmartPhone())
+                context.Response.Redirect("/root");
+            else await next();
+        }
+    }
 
-	public class ReferenceData : IReferenceData
-	{
-		public async Task Create()
-		{
-			await Context.Current.Database().Save(new PeopleService.UserInfo
-			{
-				Email = Config.Get("Authentication:SimulateLogin:Email"),
-				DisplayName = Config.Get("Authentication:SimulateLogin:DisplayName"),
-				IsActive = true,
-				ID = Config.Get("Authentication:SimulateLogin:Id").To<Guid>(),
-				Roles = Config.Get("Authentication:SimulateLogin:Roles"),
-				AuthenticatorKey = "salt",
-				Enable2FA = false,
-				OrganisationId = null
-			});
-		}
-	}
+    public class ReferenceData : IReferenceData
+    {
+        public async Task Create()
+        {
+            await Context.Current.Database().Save(new PeopleService.UserInfo
+            {
+                Email = Config.Get("Authentication:SimulateLogin:Email"),
+                DisplayName = Config.Get("Authentication:SimulateLogin:DisplayName"),
+                IsActive = true,
+                ID = Config.Get("Authentication:SimulateLogin:Id").To<Guid>(),
+                Roles = Config.Get("Authentication:SimulateLogin:Roles"),
+                AuthenticatorKey = "salt",
+                Enable2FA = false,
+                OrganisationId = null
+            });
+        }
+    }
 }
