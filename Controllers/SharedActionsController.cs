80c5b37d67939013b1aad66f547a5528ad59c899
Mohammad Hoseinzadeh Ashraf <mohammad.hoseinzadehashraf@geeks.ltd.uk>
2025-08-28 10:49:14 +0100

    healthcheck all microservices

--- Controllers/SharedActionsController.cs
+++ Controllers/SharedActionsController.cs
@@ -1,16 +1,20 @@
 ﻿namespace Controllers
 {
-    using System;
-    using System.ComponentModel;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
     using Newtonsoft.Json;
     using Olive;
     using Olive.Entities;
     using Olive.Microservices.Hub;
     using Olive.Mvc;
+    using System;
+    using System.Collections.Concurrent;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
 
     public class SharedActionsController : BaseController
     {
@@ -34,6 +38,54 @@
             return Content($"Health check @ {LocalTime.Now.ToLongTimeString()}, " +
                 $" version = {Config.Get("App.Resource.Version")} in env:{Context.Current.Environment().EnvironmentName}({Config.Get("Runtime")})," +
                 Environment.NewLine + $" email: {User.GetEmail()} roles: {User.GetRoles().ToString("|")} {Config.Get("Authentication:Cookie:Domain")}");
+        }
+
+        [Route("healthcheck/all")]
+        public async Task<ActionResult> HealthCheckAll([FromServices] IConfiguration configuration)
+        {
+            var microserviceKeys = configuration
+                .GetSection("Microservice")
+                .GetChildren()
+                .Select(x => x.GetValue<string>("Name"))
+                .ExceptNull()
+                .Cast<string>()
+                .ToArray();
+
+            var result = new ConcurrentDictionary<string, string>(microserviceKeys.ToDictionary(
+                key => key,
+                key => ""
+            ));
+
+            await Parallel.ForEachAsync(microserviceKeys, async (key, token) =>
+            {
+                try
+                {
+                    var service = Microservice.Of(key);
+                    if (service == null)
+                    {
+                        result[key] = "Not Configured";
+                        return;
+                    }
+                    var url = service.Url("healthcheck");
+                    var client = new HttpClient();
+                    var response = await client.GetStringAsync(url);
+                    if (response.HasValue())
+                        result[key] = "OK: " + response;
+                    else
+                        result[key] = "No Response";
+                }
+                catch (Exception ex)
+                {
+                    result[key] = "Error: " + ex.Message;
+                }
+            });
+
+            var html = "<h1>Health Check - All Microservices</h1><table border='1' cellpadding='5'>" +
+                "<tr><th>Microservice</th><th>Status</th></tr>" +
+                result.OrderBy(x => x.Key).Select(x => $"<tr><td>{x.Key}</td><td>{x.Value.HtmlEncode()}</td></tr>").ToString("") +
+                "</table>";
+
+            return Content(html, "text/html");
         }
 
         [Route("error")]
@@ -74,7 +126,7 @@
         public async Task<ActionResult> LocalSetup()
         {
             if (Context.Current.Environment().EnvironmentName != "Development") return Content("error-404");
-            
+
             var data = JsonConvert.DeserializeObject<LocalIncommingData>(await Request.Body.ReadAllText());
             if (data == null) return Content("Data is null");
 
